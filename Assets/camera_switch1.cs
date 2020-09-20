9d7ccb39287335256772b1489372763b305975e0
Yifan <ym2159@nyu.edu>
2020-09-20 10:34:58 -0700

    RMachine

---
+++ Assets/camera_switch1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_switch1 : MonoBehaviour
+{
+
+    public Camera camera1;
+    public Camera camera2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D ball)
+    {
+        print("camera siwtched");
+        camera1.enabled = false;
+        camera2.enabled = true;
+    }
+}
---
+++ Assets/start.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
